01053ea728b144a0ad5a9da87be5a66f28131aa5
Joerg Roth <joerg.roth@geo.uzh.ch>
2022-11-11 13:47:43 +0100

    update PWA

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import './css/inside-corona.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('service-worker.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
