1d7da69e0d58fa60881d9c7c9aef0001306a2ed5
Joerg Roth <joerg.roth@geo.uzh.ch>
2022-11-11 12:39:24 +0100

    update PWA-compatibility

--- src/index.js
+++ src/index.js
@@ -15,10 +15,9 @@ import {bfsFromNode} from 'graphology-traversal/bfs';
 import './css/inside-corona.css';
 
 if ('serviceWorker' in navigator) {
-
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('SW registered: ', registration);
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
     });
